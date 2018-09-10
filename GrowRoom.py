54c812cc919c9389ec5de9332cbd4cb0f9bd9307
Nathan Organ <nath@norgan.net>
2018-09-10 17:33:11 +1000

    fixed error
    set to only one returned value of visible light. need to now split ir

--- GrowRoom.py
+++ GrowRoom.py
@@ -136,7 +136,7 @@ def readHumidity():
         print "Error in humidity reading..."
         return -1
 
-def readLight():
+def readVisLight():
 	try:
 		# TSL2561 address, 0x39(57)
 		# Select control register, 0x00(00) with command register, 0x80(128)
@@ -160,16 +160,51 @@ def readLight():
 		# Convert the data
 		light = data[1] * 256 + data[0]
 		irlight = data1[1] * 256 + data1[0]
+		visible = light - irlight
 
 		# Output data to screen
 		print "Full Spectrum(IR + Visible) :%d lux" %light
 		print "Infrared Value :%d lux" %irlight
 		print "Visible Value :%d lux" %(light - irlight)
-		return light,irlight
+		return visible
 	except:
 		print "Error in light reading..."
         return -1
 		
+def readVisLight():
+	try:
+		# TSL2561 address, 0x39(57)
+		# Select control register, 0x00(00) with command register, 0x80(128)
+		#		0x03(03)	Power ON mode
+		bus.write_byte_data(0x39, 0x00 | 0x80, 0x03)
+		# TSL2561 address, 0x39(57)
+		# Select timing register, 0x01(01) with command register, 0x80(128)
+		#		0x02(02)	Nominal integration time = 402ms
+		bus.write_byte_data(0x39, 0x01 | 0x80, 0x02)
+
+		time.sleep(0.5)
+
+		# Read data back from 0x0C(12) with command register, 0x80(128), 2 bytes
+		# ch0 LSB, ch0 MSB
+		data = bus.read_i2c_block_data(0x39, 0x0C | 0x80, 2)
+
+		# Read data back from 0x0E(14) with command register, 0x80(128), 2 bytes
+		# ch1 LSB, ch1 MSB
+		data1 = bus.read_i2c_block_data(0x39, 0x0E | 0x80, 2)
+
+		# Convert the data
+		light = data[1] * 256 + data[0]
+		irlight = data1[1] * 256 + data1[0]
+		visible = light - irlight
+
+		# Output data to screen
+		print "Full Spectrum(IR + Visible) :%d lux" %light
+		print "Infrared Value :%d lux" %irlight
+		print "Visible Value :%d lux" %(light - irlight)
+		return light
+	except:
+		print "Error in light reading..."
+        return -1
 # Take a picture with the current time using the Raspberry Pi camera. Save it in the same folder
 
 
@@ -222,7 +257,7 @@ def main():
         },
 		"Light": {
             "type": "numeric",
-            "bind": readLight
+            "bind": readVisLight()
         }
     }
 
